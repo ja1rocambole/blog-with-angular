5ee8001cdbaf09bd55405c655a5e59f53d0e8ab6
João Fernandes <j.fernandes8077@gmail.com>
2024-02-21 20:58:18 -0300

    feat: add componentes and styles but without  logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TitleBlogComponent } from './components/title-blog/title-blog.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TitleBlogComponent, BigCardComponent, SmallCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'blog-with-angular';
-}
+export class AppComponent {}
---
+++ src/app/components/title-blog/title-blog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-blog',
+  standalone: true,
+  imports: [],
+  templateUrl: './title-blog.component.html',
+  styleUrl: './title-blog.component.css'
+})
+export class TitleBlogComponent {
+
+}
